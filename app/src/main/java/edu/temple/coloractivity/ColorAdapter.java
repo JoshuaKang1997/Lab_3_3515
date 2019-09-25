fa9eff81c1a214d35b248c610151d89f451d4f04
JoshuaKang1997 <joshkang1997@gmail.com>
2019-09-25 10:25:23 -0400

    added ColorAdapter Class

---
+++ app/src/main/java/edu/temple/coloractivity/ColorAdapter.java
@@ -0,0 +1,41 @@
+package edu.temple.coloractivity;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Spinner;
+
+public class ColorAdapter extends BaseAdapter {
+
+    String[] colors;
+    Context context;
+
+    public ColorAdapter(Context context, String colors[]) {
+        this.context = context;
+        this.colors = colors;
+    }
+
+
+    @Override
+    public int getCount() {
+        return colors.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return colors[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+}
--- app/src/main/java/edu/temple/coloractivity/MainActivity.java
+++ app/src/main/java/edu/temple/coloractivity/MainActivity.java
@@ -2,7 +2,13 @@ package edu.temple.coloractivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] colors = {"Red", "Green", "Blue", "Magenta", "Cyan", "Black", "Yellow", "Gray",
+        "Brown", "Lime"};
     }
 }
